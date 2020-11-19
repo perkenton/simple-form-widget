959bb6696b25be74320b8f21c7998ee0d7252b6b
Alexander Vasiliev <selado@ya.ru>
2020-11-19 19:37:08 +0300

    add contacts list in select from arr

---
+++ src/scripts/constants.js
@@ -0,0 +1,5 @@
+export {
+  CONTACTS_API_URL,
+};
+
+const CONTACTS_API_URL = 'https://reqres.in/api/users?page=1';
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,64 @@
 import '../pages/index.css';
-
+import {
+  CONTACTS_API_URL,
+} from './constants.js';
 
 'use strict';
 
 (function() {
 
+  // current date to input
+  Date.prototype.toDateInputValue = (function() {
+    const local = new Date(this);
+    local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+    return local.toJSON().slice(0,10);
+  });
+
+  document.querySelector('.form__calendar').value = new Date().toDateInputValue();
+  document.querySelector('.form__calendar').setAttribute('min', `${new Date().toDateInputValue()}`);
+
+
+  // get contacts list and add to dom
+  const addContact = (firstName, lastName) => {
+    const markup = `
+      <option class="form__contacts-person-item">${firstName} ${lastName}</option>
+    `;
+    const select = document.querySelector('.form__contacts-person');
+    select.insertAdjacentHTML('beforeend', markup);
+  }
+
+  const fetchPromise = () => {
+    return fetch(CONTACTS_API_URL)
+
+      .then(res => {
+        if(res.ok) {
+          return res.json();
+        }
+
+        return Promise.reject(new Error(`${res.status}, ${res.statusText}`));
+
+      })
+  }
+
+  fetchPromise()
+    .then(res => {
+      const arr = res.data;
+      arr.forEach(item => {
+        addContact(item.first_name, item.last_name);
+      })
+
+    })
+
+    .catch((err) => {
+      console.log(err);
+    });
+
+
+  // modal window
+  const modalWindow = () => {
+    const markup = `
+      
+    `;
+  }
 
 })();
