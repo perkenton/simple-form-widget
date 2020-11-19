3f680aa4ca295cdf56014330e85cd6df0e1f6d29
Alexander Vasiliev <selado@ya.ru>
2020-11-20 00:05:58 +0300

    start form validation

--- src/scripts/constants.js
+++ src/scripts/constants.js
@@ -1,5 +1,16 @@
 export {
   CONTACTS_API_URL,
+  PERSON_INPUT,
+  DATE_INPUT,
+  COMMENT_INPUT,
+  SUBMIT_INPUT,
+  CONTACTS_FORM,
 };
 
 const CONTACTS_API_URL = 'https://reqres.in/api/users?page=1';
+
+const PERSON_INPUT = document.querySelector('.form__contacts-person');
+const DATE_INPUT = document.querySelector('.form__calendar');
+const COMMENT_INPUT = document.querySelector('.form__input-text');
+const SUBMIT_INPUT = document.querySelector('.form__input-submit');
+const CONTACTS_FORM = document.querySelector('.form__contacts-person');
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,11 @@
 import '../pages/index.css';
 import {
   CONTACTS_API_URL,
+  PERSON_INPUT,
+  DATE_INPUT,
+  COMMENT_INPUT,
+  SUBMIT_INPUT,
+  CONTACTS_FORM,
 } from './constants.js';
 
 'use strict';
@@ -14,8 +19,8 @@ import {
     return local.toJSON().slice(0,10);
   });
 
-  document.querySelector('.form__calendar').value = new Date().toDateInputValue();
-  document.querySelector('.form__calendar').setAttribute('min', `${new Date().toDateInputValue()}`);
+  // DATE_INPUT.value = new Date().toDateInputValue();
+  DATE_INPUT.setAttribute('min', `${new Date().toDateInputValue()}`);
 
 
   // get contacts list and add to dom
@@ -55,10 +60,61 @@ import {
 
 
   // modal window
-  const modalWindow = () => {
-    const markup = `
-      
-    `;
+
+  const setSubmitButtonState = (state) => {
+    if(state === false) {
+      SUBMIT_INPUT.setAttribute('disabled', 'disabled');
+      SUBMIT_INPUT.classList.add('form__input-submit_is-inactive');
+    } else {
+      SUBMIT_INPUT.removeAttribute('disabled');
+      SUBMIT_INPUT.classList.remove('form__input-submit_is-inactive');
+    }
   }
+
+  const submitValidation = () => {
+    if(PERSON_INPUT.value === 'Person list' || DATE_INPUT.value === '') {
+      setSubmitButtonState(false);
+    } else {
+      setSubmitButtonState(true);
+    }
+  }
+
+  // const addListener = () => {
+  //   CONTACTS_FORM.addEventListener('input', submitValidation,true);
+  // }
+  //
+  // addListener();
+
+  CONTACTS_FORM.addEventListener('input', (event) => {
+    event.preventDefault();
+  
+    document.querySelector('.modal-window__contact').textContent = PERSON_INPUT.value;
+    document.querySelector('.modal-window__date').textContent = DATE_INPUT.value;
+    document.querySelector('.modal-window__comment').textContent = COMMENT_INPUT.value;
+
+    document.querySelector('.modal-window').classList.add('modal-window_is-opened');
+  });
+
+  // const submit = (event) => {
+  //   event.preventDefault();
+  //
+  //   submitValidation();
+  //
+  //   document.querySelector('.modal-window__contact').textContent = PERSON_INPUT.value;
+  //   document.querySelector('.modal-window__date').textContent = DATE_INPUT.value;
+  //   document.querySelector('.modal-window__comment').textContent = COMMENT_INPUT.value;
+  //
+  //   document.querySelector('.modal-window').classList.add('modal-window_is-opened');
+  // }
+
+  // CONTACTS_FORM.addEventListener('submit', submit);
+
+  document.querySelector('.modal-window__close').addEventListener('click', () => {
+    PERSON_INPUT.value = 'Person list';
+    DATE_INPUT.value = new Date().toDateInputValue();
+    COMMENT_INPUT.value = '';
+    setSubmitButtonState(false);
+    document.querySelector('.modal-window').classList.remove('modal-window_is-opened');
+  });
 
 })();
